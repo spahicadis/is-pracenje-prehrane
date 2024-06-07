db58213e97da7d8e747153a68272db563a24a570
spahicadis <142044354+spahicadis@users.noreply.github.com>
2024-06-07 19:28:25 +0200

    Fixed datetime function

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ def vizualizacija():
     return render_template('vizualizacija.html', podaci=podaci) 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
